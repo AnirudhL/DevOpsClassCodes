d3dda96034219f52c978dbe0259dcabd589796bc
AnirudhL <patil.anirudh8@gmail.com>
2021-02-10 18:24:12 +0530

    Update AddressbookUI.java

--- src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
@@ -25,7 +25,7 @@ import com.vaadin.v7.ui.TextField;
  * the same instance, add @PreserveOnRefresh.
  */
 @Title("Addressbook")
-@Theme("metro")
+@Theme("valo")
 @Widgetset("com.vaadin.v7.Vaadin7WidgetSet")
 public class AddressbookUI extends UI {
 
